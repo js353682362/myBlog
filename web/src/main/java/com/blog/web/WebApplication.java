ffee311f2a03b7e69f3a3e8bf947b20f4408539b
353682362 <353682362@qq.com>
2017-07-04 14:34:34 +0800

    web

---
+++ web/src/main/java/com/blog/web/WebApplication.java
@@ -0,0 +1,14 @@
+package com.blog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.blog.web")
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+}
