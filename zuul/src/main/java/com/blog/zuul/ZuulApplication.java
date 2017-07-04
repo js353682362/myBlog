128e2aba5da97f38165ad277067ee89a2396f865
353682362 <353682362@qq.com>
2017-07-04 11:10:15 +0800

    test

--- zuul/src/main/java/com/blog/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/blog/zuul/ZuulApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZuulApplication {
 
 	public static void main(String[] args) {
+		String s = "";
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 }
