b2429462b984b133f2b498a7b237152a93d389cd
js353682362 <353682362@qq.com>
2017-07-04 10:41:50 +0800

    Update SystemApplication.java

--- system/src/main/java/com/blog/system/SystemApplication.java
+++ system/src/main/java/com/blog/system/SystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SystemApplication {
 
 	public static void main(String[] args) {
+		String s = "123";
 		SpringApplication.run(SystemApplication.class, args);
 	}
 }
