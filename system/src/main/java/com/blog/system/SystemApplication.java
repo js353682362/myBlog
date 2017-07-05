cd603fd1fc4cd65e8107bfb05d1d2154971e4636
353682362 <353682362@qq.com>
2017-07-05 14:45:59 +0800

    web

--- system/src/main/java/com/blog/system/SystemApplication.java
+++ system/src/main/java/com/blog/system/SystemApplication.java
@@ -1,13 +1,27 @@
 package com.blog.system;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class SystemApplication {
 
 	public static void main(String[] args) {
-		String s = "123";
 		SpringApplication.run(SystemApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	private String port;
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name){
+		return "hi" + name + ",i am from port:" + port;
 	}
 }
